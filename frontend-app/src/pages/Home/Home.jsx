9344c3e19477a931d7d7dd4fe8c8900533ea8f59
fulprimitiv <eremeevdima51@gmail.com>
2025-05-29 20:37:51 +0500

    Добавил компоненты стартового экрана

---
+++ frontend-app/src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import WelcomeComponent from '../../components/WelcomeComponent/WelcomeComponent';
+import NewsComponent from '../../components/NewsComponent/NewsComponent';
+
+const Home = () => {
+  return (
+    <>
+      <WelcomeComponent />
+      <NewsComponent />
+    </>
+  );
+};
+
+export default Home;
