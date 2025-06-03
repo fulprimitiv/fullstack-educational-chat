aeffbe0d8b44eb1fc97eb6855272b0308415c9c4
fulprimitiv <eremeevdima51@gmail.com>
2025-06-03 06:42:55 +0500

    Изменил структуру ветки

--- frontend-app/src/store/types/actionTypes.js
+++
@@ -1,4 +0,0 @@
-// Типы действий Redux
-export const FETCH_DATA_REQUEST = 'FETCH_DATA_REQUEST';
-export const FETCH_DATA_SUCCESS = 'FETCH_DATA_SUCCESS';
-export const FETCH_DATA_FAILURE = 'FETCH_DATA_FAILURE';
